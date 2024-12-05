11d0fd060cd73dbdfddeefad4e06fa92145f2261
Bahar Hamzeh <blikebahar76@gmail.com>
2024-12-05 13:47:45 -0600

    Update INSTALLED_APPS and DIRS in 'settings.py' for core app and template directory.

--- social_book/settings.py
+++ social_book/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'social_book.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
